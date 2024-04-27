b1047930e7b3153d0c6196b328bb30980cb1d9ea
igorct1 <igorct77@gmail.com>
2024-04-26 22:18:11 -0300

    feat: edit and list meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -5,6 +5,7 @@ import { knex } from '../database'
 import { randomUUID } from 'crypto'
 
 export async function mealsRoutes(app: FastifyInstance) {
+  // new
   app.post(
     '/',
     {
@@ -46,7 +47,7 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(201).send()
     },
   )
-
+  // all
   app.get(
     '',
     {
@@ -62,7 +63,7 @@ export async function mealsRoutes(app: FastifyInstance) {
 
       if (!user) {
         return reply.status(404).send({
-          message: 'You need to create an user to get meals information',
+          message: 'You need to create an user to get meals information.',
         })
       }
 
@@ -71,6 +72,105 @@ export async function mealsRoutes(app: FastifyInstance) {
       reply.status(200).send({
         meals,
       })
+    },
+  )
+  // specific
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionExists],
+    },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const { sessionId } = request.cookies
+
+      const user = await knex('users')
+        .select('id')
+        .where('session_id', sessionId)
+        .first()
+
+      if (!user) {
+        return reply.status(404).send({
+          message: 'You need an active session to edit this meal.',
+        })
+      }
+
+      const mealParamsValidationSchema = z.object({
+        id: z.string(),
+      })
+
+      const { id } = mealParamsValidationSchema.parse(request.params)
+
+      const selectedMeal = await knex('meals')
+        .select('*')
+        .where('user_id', user.id)
+        .where('id', id)
+        .first()
+
+      if (selectedMeal?.user_id !== user.id)
+        return reply.status(404).send({
+          message: "You're trying to access a meal u are not the owner.",
+        })
+
+      return reply.status(200).send({
+        selectedMeal,
+      })
+    },
+  )
+
+  // edit specific
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkSessionExists],
+    },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const { sessionId } = request.cookies
+
+      const user = await knex('users')
+        .select('*')
+        .where('session_id', sessionId)
+        .first()
+
+      if (!user) {
+        return reply.status(404).send({
+          message: 'You need an active session to edit this meal.',
+        })
+      }
+
+      const mealsBodyValidationSchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        isOnDiet: z.boolean().optional(),
+      })
+
+      const mealsParamsValidationSchema = z.object({
+        id: z.string(),
+      })
+
+      const { description, isOnDiet, name } = mealsBodyValidationSchema.parse(
+        request.body,
+      )
+      const { id } = mealsParamsValidationSchema.parse(request.params)
+
+      const selectedMealToEdit = await knex('meals')
+        .where('id', id)
+        .select('user_id')
+        .first()
+
+      if (user.id !== selectedMealToEdit?.user_id) {
+        return reply.status(404).send({
+          message: "You're trying to edit an meal that u are not allowed to.",
+        })
+      }
+
+      await knex('meals').where('id', id).update({
+        name,
+        description,
+        isOnDiet,
+        updated_at: new Date(),
+      })
+
+      return reply.status(200).send()
     },
   )
 }
